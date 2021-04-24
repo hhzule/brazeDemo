44c5d891de6be1a8efaf4434f0c6391274aa2a7c
hhzule <hhzule@gmail.com>
2021-04-24 05:10:12 +0500

    final commit

--- lambda/index.ts
+++ lambda/index.ts
@@ -18,15 +18,11 @@ type AppSyncEvent = {
 
 exports.handler = async (event: AppSyncEvent) => {
 
-const notesArray = ["note1", "note2", "note3"]
-
     switch(event.info.fieldName){
         case "productQuery":
             return "base query ran";
         case "addProduct":
             const result = await brazeFn();
-            console.log(result)
-            // return "addProduct ran"
             return {
                ...event.arguments.objInput,
                messageStatus: "true",
